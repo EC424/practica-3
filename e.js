bc109ee2d709143f225c360e8a3fe42129f531b1
EC424 <eduardo.casiano.348@gmail.com>
2022-04-20 11:55:19 -0500

    cabios en js

--- e.js
+++ e.js
@@ -1,5 +1,18 @@
-var e=3;
-switch(e){
-	case 3:alert('es 3');break;
-	default:alert('dato no encontrado');
+var constructor=function(nombreinicial){
+	var obj={
+		nombre:nombreinicial,
+		edad:30,
+		mostrardatos:function(){
+			alert(this.nombre);
+		},
+		cambiardatos:function(nombrenuevo){
+			this.nombre=nombrenuevo;
+		},
+	};
+	return obj;
 };
+var empleado1=constructor('eduardo');
+var empleado2=constructor('Gonzales');
+var empleado3=constructor('Gamarra');
+alert(empleado1.nombre+'\n'+empleado2.nombre+'\n'+empleado3.nombre);
+alert(constructor('nombre1').nombre+'\n'+constructor('nombre2').nombre+'\n'+constructor('nombre3').nombre);
