3468e095db5acef5e434d9cf83d94a49222e256e
EC424 <eduardo.casiano.348@gmail.com>
2022-04-27 13:21:34 -0500

    tema del dia constructores

--- e.js
+++ e.js
@@ -1,18 +1,19 @@
-var constructor=function(nombreinicial){
-	var obj={
-		nombre:nombreinicial,
-		edad:30,
-		mostrardatos:function(){
-			alert(this.nombre);
-		},
-		cambiardatos:function(nombrenuevo){
-			this.nombre=nombrenuevo;
-		},
-	};
-	return obj;
+var constructor=function(){
+	        var nombre='edu';
+         	var edad=21;
+         var obj={
+         	
+         	mostrar:function(){
+         		alert(nombre)
+         	},
+         	cambiar:function(valor){
+                 nombre=valor
+         	},
+         };
+         return obj;
 };
-var empleado1=constructor('eduardo');
-var empleado2=constructor('Gonzales');
-var empleado3=constructor('Gamarra');
-alert(empleado1.nombre+'\n'+empleado2.nombre+'\n'+empleado3.nombre);
-alert(constructor('nombre1').nombre+'\n'+constructor('nombre2').nombre+'\n'+constructor('nombre3').nombre);
+var objeto1=constructor();
+objeto1.cambiar('melissa');
+objeto1.mostrar();
+
+
