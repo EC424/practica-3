fd8367f3439bcb184e59c1e5c294e0772a06c88d
EC424 <eduardo.casiano.348@gmail.com>
2022-04-27 14:50:45 -0500

    terminando el tema de herancia

--- e.js
+++ e.js
@@ -1,19 +1,4 @@
-var constructor=function(){
-	        var nombre='edu';
-         	var edad=21;
-         var obj={
-         	
-         	mostrar:function(){
-         		alert(nombre)
-         	},
-         	cambiar:function(valor){
-                 nombre=valor
-         	},
-         };
-         return obj;
-};
-var objeto1=constructor();
-objeto1.cambiar('melissa');
-objeto1.mostrar();
+
+
 
 
