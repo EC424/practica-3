68e8fe8e0e0905c045251f26be7ee8624f0c6e87
EC424 <eduardo.casiano.348@gmail.com>
2022-04-30 10:30:42 -0500

    contructor especial new

--- e.js
+++ e.js
@@ -1,4 +1,17 @@
+var edgardo={
+	nombre:'edgardo',
+	edad:23,
+	mostrar:function(){
+		alert(this.nombre)
+	},
+};
+var empleado=Object.create(edgardo);
+empleado.mostrar();
+empleado.cambiar=function(nuevonombre){
+	this.nombre=nuevonombre;
+};
+empleado.cambiar('luis');
+empleado.mostrar();
 
-
-
-
+var empleado2=Object.create(empleado);
+empleado2.mostrar;
