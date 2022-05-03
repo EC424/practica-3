7618421a350b432eaf9389dca171774fbf4ccb23
EC424 <eduardo.casiano.348@gmail.com>
2022-05-02 21:56:41 -0500

    nuevo dato

--- e.js
+++ e.js
@@ -1,17 +1,62 @@
-var edgardo={
-	nombre:'edgardo',
-	edad:23,
+//OBJETOS
+/*NOTACION LITERAL DE DECLARACION DE UN OBJETO
+ var obj={
+	nombre:'eduardo',
+	edad:30,
 	mostrar:function(){
-		alert(this.nombre)
+	  alert(this.nombre);
 	},
-};
-var empleado=Object.create(edgardo);
-empleado.mostrar();
-empleado.cambiar=function(nuevonombre){
-	this.nombre=nuevonombre;
-};
-empleado.cambiar('luis');
-empleado.mostrar();
+ };
+ obj.mostrar();
+ */
 
-var empleado2=Object.create(empleado);
-empleado2.mostrar;
+/*CONTRUCTORES
+  Un contructor es usado para crear copias del objeto que contienen , los contructores se declaran como 
+  funciones anónimas.
+  var contructor=function(){
+	var obj={
+	   nombre:'eduardo',
+	   edad:30,
+	   mostrar:function(){
+	      alert(this.nombre)
+	   },
+	};
+  };
+  var objeto1=contructor();
+  objeto1.mostrar();
+   */
+
+/*OPERADOR NEW
+  Es un tipo de contructor especial, se define el objeto como una funcion y para crear un objeto de usa new ;
+  se usara this parareferenciar todo dentro de la funcion objeto .
+  function miobjeto(){
+	this.nombre='eduardo';
+	this.edad=30;
+	mostrar=function(){
+	   alert(this.nombre);
+	};
+  };
+  var empeado= new miobjeto();
+  empleado.mostrar();
+  */
+
+/*HERENCIA
+  Se le llama asi por que los objetos que siguen en la linea e¡heredan las propiedades y metodos 
+  del objeto prototipo que contiene "Object.create(prototipo de objeto)"
+  ejem:
+
+  var objeto={
+	nombre:'eduardo',
+	mostrar:function(){
+	   alert(this.nombre);
+	},
+  };
+  var empleado=Object.create(objeto);
+  empleado.nombre='Santiago'
+  empleado.mostrar();
+
+  NOTA:empleado es el nuevo objeto q¡creado y el cual hereda todas las propiedades y métodos del objeto que en 
+  este caso hace el papel de protito para la cracion de otrso objetos , si creamos otro objeto y este usa de 
+  prototipo a empleado, todos los cambios echos en empleado que lo diferencian de objeto los heredara dicho 
+  objeto.
+  */
